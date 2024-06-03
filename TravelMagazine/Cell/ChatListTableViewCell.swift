2eb18b5c046e5fb8f8049f5fa62827a924d5c8a2
JunYeong-J <eeot2827@gmail.com>
2024-06-03 09:03:12 +0900

    TRAVEL톡 화면 구성

---
+++ TravelMagazine/Cell/ChatListTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  ChatListTableViewCell.swift
+//  TravelMagazine
+//
+//  Created by 전준영 on 6/3/24.
+//
+
+import UIKit
+
+class ChatListTableViewCell: UITableViewCell {
+
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var contentsLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureLayout()
+    }
+    
+    func configureLayout() {
+        
+        nicknameLabel.setPrimaryLabel(textColor: .black, font: .boldSystemFont(ofSize: 15), textAlignment: .left, numberOfLines: 1)
+        contentsLabel.setPrimaryLabel(textColor: .darkGray, font: .systemFont(ofSize: 13), textAlignment: .left, numberOfLines: 1)
+        dateLabel.setPrimaryLabel(textColor: .lightGray, font: .systemFont(ofSize: 11), textAlignment: .right, numberOfLines: 1)
+        profileImage.imageViewUI(contentMode: .scaleAspectFill, cornerRadius: 25)
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func configureCell(data: ChatRoom) {
+        profileImage.image = UIImage(named: data.chatroomImage[0])
+        nicknameLabel.text = data.chatroomName
+        contentsLabel.text = data.chatList.last?.message
+        dateLabel.text = data.chatList.last?.date
+    }
+}
---
+++ TravelMagazine/VC/ChatViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ChatViewController.swift
+//  TravelMagazine
+//
+//  Created by 전준영 on 6/3/24.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    let chatList = mockChatList
+    var newList:[ChatRoom] = []
+    
+    @IBOutlet var nicknameSearchBar: UISearchBar!
+    @IBOutlet var chatListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationUI()
+        configureUI()
+        configureTable()
+    }
+    
+    
+    
+}
+
+extension ChatViewController {
+    
+    func navigationUI() {
+        
+        self.navigationTitle("TRAVEL TALK")
+        
+    }
+    
+    
+    func configureUI() {
+        
+        nicknameSearchBar.placeholder = "친구 이름을 검색해보세요"
+        nicknameSearchBar.tintColor = .black
+        nicknameSearchBar.delegate = self
+        
+        newList = chatList
+    }
+    
+    func configureTable() {
+        chatListTableView.delegate = self
+        chatListTableView.dataSource = self
+        let xib = UINib(nibName: ChatListTableViewCell.identifier, bundle: nil)
+        chatListTableView.register(xib, forCellReuseIdentifier: ChatListTableViewCell.identifier)
+        chatListTableView.rowHeight = 80
+    }
+    
+}
+
+extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ChatListTableViewCell.identifier, for: indexPath) as! ChatListTableViewCell
+        cell.configureCell(data: chatList[indexPath.row])
+        return cell
+    }
+    
+}
+
+extension ChatViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        var searchList: [ChatRoom] = []
+        
+        for item in chatList{
+            if item.chatroomName.contains(searchBar.text!) {
+                searchList.append(item)
+            }
+        }
+        newList = searchList
+        chatListTableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        newList = chatList
+        chatListTableView.reloadData()
+    }
+    
+}
